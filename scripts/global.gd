653af5a914a0f6de02a8ea13ae8bfb664eee5f3a
Goonchii <trashal666@gmail.com>
2026-01-01 17:55:45 +0000

    Initial commit

---
+++ scripts/global.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var start = "Starting game."
+
+var AI = {
+	"Char1": 20
+}
+
+var Char1Pos = "start"
---
+++ scripts/movement.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+func _ready() -> void:
+	print(Global.start)
+	print("Char1 is at the: ", Global.Char1Pos)
+	$Timer.timeout.connect(timeout)
+
+func timeout() -> void:
+	if randi_range(1,20) <= Global.AI["Char1"]:
+		print("Char1 moved.")
+		move()
+		print("Char1 is in: ", Global.Char1Pos)
+		
+		if Global.Char1Pos == "door":
+			$Timer.stop()
+			print("Char1 is at the door.")
+
+func move() -> void:
+		match Global.Char1Pos:
+			"start":
+				Global.Char1Pos = ["room1", "room2"].pick_random()
+			"room1":
+				Global.Char1Pos = ["room3", "room4"].pick_random()
+			"room2":
+				Global.Char1Pos = ["room5", "door"].pick_random()
+			"room3":
+				Global.Char1Pos = ["room2"].pick_random()
+			"room4":
+				Global.Char1Pos = ["room2"].pick_random()
+			"room5":
+				Global.Char1Pos = ["room2"].pick_random()
