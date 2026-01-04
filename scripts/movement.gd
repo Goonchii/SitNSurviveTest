9ece90aa6b06cec83b85ecc85e194372f81096b3
Goonchii <trashal666@gmail.com>
2026-01-04 00:35:33 +0000

    Updated Character AI
    AI level now changes for each night
    Removed breaking system (will be replaced later)

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -1,34 +1,36 @@
 extends Node2D
 
+var Char1Pos = "start"
 # Runs on start
 func _ready() -> void:
-	print("Char1 is at the: ", Global.Char1Pos)
+	print("Char1 is at the: ", Char1Pos)
+	print("Char1 AI lvl is ", Global.AI["Char1"][Global.currentNight - 1])
 	$Timer.timeout.connect(timeout)
 
 # Movement Opportunity
 func timeout() -> void:
-	if randi_range(1,20) <= Global.AI["Char1"]:
+	if randi_range(1,20) <= Global.AI["Char1"][Global.currentNight - 1]:
 		print("Char1 moved.")
 		move()
-		print("Char1 is in: ", Global.Char1Pos)
+		print("Char1 is in: ", Char1Pos)
 		
 		# Stop when they reach the door
-		if Global.Char1Pos == "door":
+		if Char1Pos == "door":
 			$Timer.stop()
 			print("Char1 is at the door.")
 
 # Movement path
 func move() -> void:
-		match Global.Char1Pos:
+		match Char1Pos:
 			"start":
-				Global.Char1Pos = ["room1", "room2"].pick_random()
+				Char1Pos = ["room1", "room2"].pick_random()
 			"room1":
-				Global.Char1Pos = ["room3", "room4"].pick_random()
+				Char1Pos = ["room3", "room4"].pick_random()
 			"room2":
-				Global.Char1Pos = ["room5", "door"].pick_random()
+				Char1Pos = ["room5", "door"].pick_random()
 			"room3":
-				Global.Char1Pos = ["room2"].pick_random()
+				Char1Pos = ["room2"].pick_random()
 			"room4":
-				Global.Char1Pos = ["room2"].pick_random()
+				Char1Pos = ["room2"].pick_random()
 			"room5":
-				Global.Char1Pos = ["room2"].pick_random()
+				Char1Pos = ["room2"].pick_random()
--- scripts/testcharacter.gd
+++ scripts/testcharacter.gd
@@ -2,19 +2,18 @@ extends Node2D
 
 var Char2Pos = "start"
 var Char2State = "normal"
-var faultyProgress = 0
 
 # Runs on start
 func _ready() -> void:
 	print("Char2 is at the: ", Char2Pos)
+	print("Char2 AI lvl is ", Global.AI["Char2"][Global.currentNight - 1])
 	$Timer.timeout.connect(timeout)
 
 # Movement Opportunity
 func timeout() -> void:
-	if randi_range(1,20) <= Global.AI["Char2"]:
+	if randi_range(1,20) <= Global.AI["Char2"][Global.currentNight - 1]:
 		print("Char2 moved.")
 		move()
-		breaking()
 		print("Char2 is in: ", Char2Pos)
 		
 		# Stop if in office and is faulty
@@ -49,20 +48,3 @@ func move() -> void:
 			"office":
 				Char2Pos = ["start"].pick_random()
 				
-func breaking() -> void:
-	if Char2State == "faulty" or Char2Pos == "office":
-		print("[BREAKING] Skipped | State:", Char2State, " Pos:", Char2Pos)
-		return
-	
-	var chance = Global.AI["Char2"] + faultyProgress
-	var roll = randi_range(1,100)
-
-	print("[BREAKING] Roll:", roll, "/", 100,
-		  " Chance:", chance,
-		  " Progress:", faultyProgress)
-
-	if roll <= chance:
-		print(">>> [FAULTY TRIGGERED]")
-		Char2State = "faulty"
-	else:
-		faultyProgress += 1
