cb9c5cf47430cf01210f3c096db8a361ca4b85bc
Goonchii <trashal666@gmail.com>
2026-03-19 19:44:51 +0000

    Chars appear on cams and shows their movements

---
+++ scripts/cams_sprite_manager.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+@onready var char1 = get_node("/root/Office/Char1")
+@onready var char2 = get_node("/root/Office/Char2")
+
+# Sprites per character per room
+@export var char1_stage: Sprite2D
+@export var char1_dining: Sprite2D
+@export var char1_backstage: Sprite2D
+@export var char1_kitchen: Sprite2D
+@export var char1_lefthall: Sprite2D
+@export var char1_righthall: Sprite2D
+
+@export var char2_stage: Sprite2D
+@export var char2_dining: Sprite2D
+@export var char2_backstage: Sprite2D
+@export var char2_kitchen: Sprite2D
+@export var char2_lefthall: Sprite2D
+@export var char2_righthall: Sprite2D
+
+var char1_sprites: Dictionary = {}
+var char2_sprites: Dictionary = {}
+
+func _ready() -> void:
+	char1_sprites = {
+		"stage":  char1_stage,
+		"dining": char1_dining,
+		"backstage": char1_backstage,
+		"kitchen": char1_kitchen,
+		"lefthall": char1_lefthall,
+		"righthall": char1_righthall
+	}
+	char2_sprites = {
+		"stage":  char2_stage,
+		"dining": char2_dining,
+		"backstage": char2_backstage,
+		"kitchen": char2_kitchen,
+		"lefthall": char2_lefthall,
+		"righthall": char2_righthall
+	}
+
+func _process(_delta: float) -> void:
+	update_character(char1, char1_sprites)
+	update_character(char2, char2_sprites)
+
+func update_character(character: Node, sprites: Dictionary) -> void:
+	for room in sprites:
+		var sprite = sprites[room]
+		if sprite != null:
+			sprite.visible = (character.char_position == room)
