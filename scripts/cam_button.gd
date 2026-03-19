4b6c321774048e116422c65e951a1c7477a7af88
Goonchii <trashal666@gmail.com>
2026-03-19 12:49:28 +0000

    Fixed bug where it didn't open the first time

--- scripts/cam_button.gd
+++ scripts/cam_button.gd
@@ -1,11 +1,9 @@
 extends Button
 
-var is_open := true
 @onready var monitor_layer: CanvasLayer = get_node("/root/Office/GUI/Monitor")
 
 func _on_mouse_entered() -> void:
-	is_open = !is_open
-	if is_open:
+	if monitor_layer.visible:
 		monitor_layer.hide()
 	else:
 		monitor_layer.show()
