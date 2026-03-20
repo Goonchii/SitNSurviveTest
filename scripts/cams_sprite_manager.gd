93f89779b66cfa5153c54143a03dba6c0eb12685
Goonchii <trashal666@gmail.com>
2026-03-20 21:51:20 +0000

    Added Freddy camera sprites

--- scripts/cams_sprite_manager.gd
+++ scripts/cams_sprite_manager.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var char1 = get_node("/root/Office/Char1")
 @onready var char2 = get_node("/root/Office/Char2")
+@onready var freddy = get_node("/root/Office/Freddy")
 
 # Sprites per character per room
 @export var char1_stage: Sprite2D
@@ -18,12 +19,20 @@ extends Node2D
 @export var char2_lefthall: Sprite2D
 @export var char2_righthall: Sprite2D
 
+@export var freddy_stage: Sprite2D
+@export var freddy_dining: Sprite2D
+@export var freddy_backstage: Sprite2D
+@export var freddy_kitchen: Sprite2D
+@export var freddy_lefthall: Sprite2D
+@export var freddy_righthall: Sprite2D
+
 var char1_sprites: Dictionary = {}
 var char2_sprites: Dictionary = {}
+var freddy_sprites: Dictionary = {}
 
 func _ready() -> void:
 	char1_sprites = {
-		"stage":  char1_stage,
+		"stage": char1_stage,
 		"dining": char1_dining,
 		"backstage": char1_backstage,
 		"kitchen": char1_kitchen,
@@ -31,17 +40,26 @@ func _ready() -> void:
 		"righthall": char1_righthall
 	}
 	char2_sprites = {
-		"stage":  char2_stage,
+		"stage": char2_stage,
 		"dining": char2_dining,
 		"backstage": char2_backstage,
 		"kitchen": char2_kitchen,
 		"lefthall": char2_lefthall,
 		"righthall": char2_righthall
 	}
+	freddy_sprites = {
+		"stage": freddy_stage,
+		"dining": freddy_dining,
+		"backstage": freddy_backstage,
+		"kitchen": freddy_kitchen,
+		"lefthall": freddy_lefthall,
+		"righthall": freddy_righthall
+	}
 
 func _process(_delta: float) -> void:
 	update_character(char1, char1_sprites)
 	update_character(char2, char2_sprites)
+	update_character(freddy, freddy_sprites)
 
 func update_character(character: Node, sprites: Dictionary) -> void:
 	for room in sprites:
