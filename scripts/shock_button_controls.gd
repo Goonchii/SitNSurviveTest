f4fdf70087187c9d6fe4f7dde8d4baf976bd0610
Goonchii <trashal666@gmail.com>
2026-03-20 20:45:19 +0000

    Added shock buttons that send chars back to start when in halls

--- scripts/shock_button_controls.gd
+++ scripts/shock_button_controls.gd
@@ -1,7 +1,12 @@
 extends Control
 
+@onready var char1 = get_node("/root/Office/Char1")
+@onready var char2 = get_node("/root/Office/Char2")
 
+func _on_l_shock_button_button_up() -> void:
+	if char1.char_position == "lefthall":
+		char1.reset_to_start()
 
-func _on_texture_button_button_up() -> void:
-	pass
-		
+func _on_r_shock_button_button_up() -> void:
+	if char2.char_position == "righthall":
+		char2.reset_to_start()
