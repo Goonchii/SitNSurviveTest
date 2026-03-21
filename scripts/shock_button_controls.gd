9f08e3e9d7b049df3163602f55ae15929743069f
Goonchii <trashal666@gmail.com>
2026-03-21 00:25:54 +0000

    Freddy kills player if they use shock buttons while he is in office

--- scripts/shock_button_controls.gd
+++ scripts/shock_button_controls.gd
@@ -4,15 +4,24 @@ var shock_counter = 0
 
 @onready var char1 = get_node("/root/Office/Char1")
 @onready var char2 = get_node("/root/Office/Char2")
+@onready var freddy = get_node("/root/Office/Freddy")
 
 func _on_l_shock_button_button_up() -> void:
-	if char1.char_position == "lefthall":
+	if freddy.char_position == "office":
+		print("Char2 attacks.")
+		Global.player_dies()
+		freddy.reset_to_start()
+	else: if char1.char_position == "lefthall":
 		char1.reset_to_start()
 		shock_counter += 1
 		print("Shock counter set to: ", shock_counter)
 
 func _on_r_shock_button_button_up() -> void:
-	if char2.char_position == "righthall":
+	if freddy.char_position == "office":
+		print("Char2 attacks.")
+		Global.player_dies()
+		freddy.reset_to_start()
+	else: if char2.char_position == "righthall":
 		char2.reset_to_start()
 		shock_counter += 1
 		print("Shock counter set to: ", shock_counter)
