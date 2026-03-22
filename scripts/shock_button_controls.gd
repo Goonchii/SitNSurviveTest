d0cf4e3e9f724b0a0e75389374de69d305231b26
Goonchii <trashal666@gmail.com>
2026-03-22 15:01:51 +0000

    Added shared cooldown timer to shock buttons
    + buttons disable on cooldown with texture to indicate

--- scripts/shock_button_controls.gd
+++ scripts/shock_button_controls.gd
@@ -1,27 +1,53 @@
 extends Control
 
-var shock_counter = 0
+var shock_counter: int = 0
+var cooldown_ready: bool = true
 
 @onready var char1 = get_node("/root/Office/Char1")
 @onready var char2 = get_node("/root/Office/Char2")
 @onready var freddy = get_node("/root/Office/Freddy")
 
-func _on_l_shock_button_button_up() -> void:
+@onready var b_shock_button = get_node("/root/Office/OfficeUI/BShockButton")
+@onready var c_shock_button = get_node("/root/Office/OfficeUI/CShockButton")
+
+func _on_b_shock_button_button_up() -> void:
 	if freddy.char_position == "office":
 		print("Freddy attacks.")
 		Global.player_dies()
 		freddy.reset_to_start()
-	else:
+		
+	else: if cooldown_ready:
+		cooldown_ready = false
+		b_shock_button.disabled = true
+		c_shock_button.disabled = true
+		$CooldownTimer.start()
+		
 		char1.reset_to_start()
+		print("Shocked bonnie.")
+		
 		shock_counter += 1
 		print("Shock counter set to: ", shock_counter)
 
-func _on_r_shock_button_button_up() -> void:
+func _on_c_shock_button_button_up() -> void:
 	if freddy.char_position == "office":
 		print("Freddy attacks.")
 		Global.player_dies()
 		freddy.reset_to_start()
-	else:
+		
+	else: if cooldown_ready:
+		cooldown_ready = false
+		b_shock_button.disabled = true
+		c_shock_button.disabled = true
+		$CooldownTimer.start()
+		
 		char2.reset_to_start()
+		print("Shocked chica.")
+		
 		shock_counter += 1
 		print("Shock counter set to: ", shock_counter)
+
+
+func _on_cooldown_timer_timeout() -> void:
+	cooldown_ready = true
+	b_shock_button.disabled = false
+	c_shock_button.disabled = false
