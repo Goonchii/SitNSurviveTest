fd5e09c410b3824db60973bd99782333de5caf85
Goonchii <trashal666@gmail.com>
2026-04-08 20:30:19 +0100

    Start button displays the correct next night (excludes custom nights)

--- scripts/StartButton.gd
+++ scripts/StartButton.gd
@@ -1,7 +1,13 @@
 extends Button
 
 func _ready() -> void:
-	text = "Start Night " + str(Global.currentNight)
+	# Iterate thru every night
+	for i in Global.completedNights.size():
+		# If the val is false (night is not complete)
+		if not Global.completedNights[i]:
+			# Add 1 to get night num (bc it starts at 0)
+			text = "Start Night " + str(i + 1)
+			return
 
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/office.tscn")
