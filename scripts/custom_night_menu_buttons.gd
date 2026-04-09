7321694830ccd8a46183a1950a79797a0af93db4
Goonchii <trashal666@gmail.com>
2026-04-09 23:33:27 +0100

    Reorganised and added back to menu button to custom night menu

--- scripts/custom_night_menu_buttons.gd
+++ scripts/custom_night_menu_buttons.gd
@@ -1,5 +1,8 @@
-extends Button
+extends VBoxContainer
 
 func _on_button_up() -> void:
 	Global.apply_custom_night()
 	get_tree().change_scene_to_file("res://scenes/office.tscn")
+
+func _on_back_button_up() -> void:
+	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
