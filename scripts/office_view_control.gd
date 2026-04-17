48cc36f26d228be0afa99107d3fa24a8a5549410
Goonchii <trashal666@gmail.com>
2026-04-17 23:07:44 +0100

    Changed controls to ASD keys (instead of buttons on screen))
    A: front office
    D: back office
    S: Toggle cams

--- scripts/office_view_control.gd
+++ scripts/office_view_control.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 @onready var view_front = $"../OfficeFront"
 @onready var view_back  = $"../OfficeBack"
-@onready var cams_button = $"../TopUI/CamButton"
 @onready var monitor_layer: CanvasLayer = get_node("/root/Office/Monitor")
 
 @onready var freddy = $"../Freddy"
@@ -13,32 +12,25 @@ func _ready() -> void:
 	facing_front = true
 	view_front.visible = true
 	view_back.visible = false
-	cams_button.visible = false
 
-func _on_turn_back_button_mouse_entered() -> void:
-	# TODO: Animation here >
-	
-	facing_front = !facing_front
-	view_front.visible = facing_front
-	view_back.visible  = !facing_front
-	cams_button.visible = !facing_front
-	
-	freddy.on_turn_back()
-
-
-func _on_turn_front_button_mouse_entered() -> void:
-	# TODO: Animation here >
-	
-	facing_front = !facing_front
-	view_front.visible = facing_front
-	view_back.visible  = !facing_front
-	cams_button.visible = !facing_front
-
-func _on_cam_button_mouse_entered() -> void:
-	if monitor_layer.visible:
-		monitor_layer.hide()
-		view_back.show()
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Akey"):
+		# TODO: Animation here >
+		facing_front = !facing_front
+		view_front.visible = facing_front
+		view_back.visible  = !facing_front
+	if Input.is_action_just_pressed("Dkey"):
+		# TODO: Animation here >
+		facing_front = !facing_front
+		view_front.visible = facing_front
+		view_back.visible  = !facing_front
+		
 		freddy.on_turn_back()
-	else:
-		monitor_layer.show()
-		view_back.hide()
+	if Input.is_action_just_pressed("Skey"):
+		if monitor_layer.visible:
+			monitor_layer.hide()
+			view_back.show()
+			freddy.on_turn_back()
+		else:
+			monitor_layer.show()
+			view_back.hide()
