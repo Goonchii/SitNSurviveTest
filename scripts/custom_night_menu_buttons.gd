6491918620803b10523dcc0ef8029d06161f0d87
Goonchii <trashal666@gmail.com>
2026-04-17 21:51:56 +0100

    Fixed start custom night button

--- scripts/custom_night_menu_buttons.gd
+++ scripts/custom_night_menu_buttons.gd
@@ -1,8 +1,8 @@
 extends VBoxContainer
 
-func _on_button_up() -> void:
-	Global.apply_custom_night()
-	get_tree().change_scene_to_file("res://scenes/office.tscn")
-
 func _on_back_button_up() -> void:
 	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+
+func _on_start_button_up() -> void:
+	Global.apply_custom_night()
+	get_tree().change_scene_to_file("res://scenes/office.tscn")
